ef6bf5d3abe50e2af932a2fb07a66b263e37e227
ISRAEL John Wesley <israeljohnwesley80@gmail.com>
2025-07-14 10:31:09 +0300

    validation finale version

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/getage", (req, res)=>
-res.json({
-  ipaddress : req.ip,
-  language : req.headers['accept-language'],
-  software: req.headers['user-agent']
-}))
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
+});
 
 
 // listen for requests :)
